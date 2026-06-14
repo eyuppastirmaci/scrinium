90aaa2f0bfa34f573df41d9cc3cafc2474d05f70
Eyüp <pastirmacieyup7@gmail.com>
2026-06-14 19:07:04 +0300

    refactor: replace Kotlin DocumentStatus enum with plain Java

---
+++ document-service/src/main/java/dev/scrinium/document/domain/model/DocumentStatus.java
@@ -0,0 +1,6 @@
+package dev.scrinium.document.domain.model;
+
+public enum DocumentStatus {
+    PENDING,
+    READY
+}
--- document-service/src/main/java/dev/scrinium/document/domain/model/DocumentStatus.kt
+++
@@ -1,6 +0,0 @@
-package dev.scrinium.document.domain.model
-
-enum class DocumentStatus {
-    PENDING,
-    READY
-}
